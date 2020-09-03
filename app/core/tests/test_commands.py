5cbd65a000ab5ed31edf9ae694c2ba7461981c34
Joe Fearnley <joef@thestowcompany.com>
2020-09-03 12:26:07 -0400

    Adding command to start database

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,20 @@
+from unittest.mock import patch
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_to_be_ready(self):
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def wait_for_db(self, ts):
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 5)
