ff1cab3dbbf55c5b7631e5a9deb65f3a59bbbd6f
Joe Fearnley <joef@thestowcompany.com>
2020-09-06 12:47:16 -0400

    Getting docker set up with postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5=1k2rx3(*@lk32nkq%!z#5i(d*r)lsyt8zvn@hli=n2#&9th!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
